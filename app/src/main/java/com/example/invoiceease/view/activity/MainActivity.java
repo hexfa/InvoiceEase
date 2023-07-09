d614df607b3992035968c7314aad2971b0784d81
@hexfa <amir.dehdarian2009@gmail.com>
2023-07-09 23:59:19 +0430

    Use Navigation Component in mainActivity and put nav_host in MainActivity.java.

--- app/src/main/java/com/example/invoiceease/view/activity/MainActivity.java
+++ app/src/main/java/com/example/invoiceease/view/activity/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.invoiceease.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.invoiceease.R;
+import com.example.invoiceease.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
